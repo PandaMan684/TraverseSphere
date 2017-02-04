6fcb40599d6fb5247362e792703d796ac31093b9
adamjamboys <adamjamboys@gmail.com>
2017-02-04 14:44:51 -0800

    Add JS (2)

---
+++ js/script.js
@@ -0,0 +1,4 @@
+System.out.println("")
+arr[] arr = new arr{UnitedArabEmirates, UnitesStatesofAmerica, UnitedKindom,  China}
+String[] UnitedArabEmirates = new arr{};
+String[] UnitedStatesOfAmerica = new arr{};
