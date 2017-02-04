b38ffd3601e5e0de6ef8b8aee4027b83482d5f55
adamjamboys <adamjamboys@gmail.com>
2017-02-04 14:43:45 -0800

    Revert "add javascript"
    This reverts commit 7ea274ddb42f8c6f52dfbf5f48c47b10a23f8f27.

--- js/script.js
+++
@@ -1,4 +0,0 @@
-System.out.println("")
-arr[] arr = new arr{UnitedArabEmirates, UnitesStatesofAmerica, UnitedKindom,  China}
-String[] UnitedArabEmirates = new arr{};
-String[] UnitedStatesOfAmerica = new arr{};
