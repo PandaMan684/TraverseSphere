a691e27682379de676bc89b72ddccc220e4383bf
adamjamboys <adamjamboys@gmail.com>
2017-02-04 15:39:17 -0800

    Added JS Germany

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-System.out.println("")
-arr[] arr = new arr{UnitedArabEmirates, UnitesStatesofAmerica, UnitedKindom,  China}
+System.out.println("Countries");
+arr[] arr = new arr{UnitedArabEmirates, UnitesStatesofAmerica, UnitedKindom, China, Japan, Sapin, Korea, Canada, France, Germany}
 String[] UnitedArabEmirates = new arr{};
 String[] UnitedStatesOfAmerica = new arr{};
+String[] UnitedKingdom = new arr{};
+String[] China = {};
+String[] Japan = {};
+String[] Sapin = {};
+String[] Korea = {};
+String[] Canada = {};
+String[] France = {};
+String[] Germany = {"Dining Etiquette: \n 1) if invited to dinner at someone’s home, bring a gift for hosts. \n 2) Do not arrive late to events. \n 3) Hold fork in left hand and knife in right hand for whole meal.",
+                + "Driving Style: Drive on the right, overtake on the left, no speed limit on autobahn (freeway)", 
+                + "Local Traditions/Events: \n 1)Day of German Unity: a national holiday commemorating reunification of East and West Germanys on October 3, 1990. \n 2)Oktoberfest: a traditional festival that lasts two weeks in October. People dress in traditional clothing, and enjoy music, dancing and drinking.", 
+                + "Currency exchange: 1 US dollar is 0.93 Euro."};
